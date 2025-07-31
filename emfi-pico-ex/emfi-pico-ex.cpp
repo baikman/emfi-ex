9a2c1b1ac3ce7443bd1a74eb41b00c3ab048efd6
baikman <baikman@cedarville.edu>
2025-07-31 10:20:27 -0400

    Basic EMFI pico update

--- emfi-pico-ex/emfi-pico-ex.cpp
+++ emfi-pico-ex/emfi-pico-ex.cpp
@@ -1,13 +1,41 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
 
+#define LED_PIN 25
 
-int main()
-{
+int main() {
     stdio_init_all();
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    bool ledVal = false;
+
+    const uint32_t magic = 0xDEADBEEF;
 
     while (true) {
-        printf("Hello, world!\n");
+        if (magic != 0xDEADBEEF) {
+            break;
+        }
+
+        if (ledVal) {
+            gpio_put(LED_PIN, 1);
+        } else {
+            gpio_put(LED_PIN, 0);
+        }
+
+        ledVal = !ledVal;
+
+        printf("Still running...\n");
         sleep_ms(1000);
+    }
+
+    // Control flow faulted via EMFI!!!
+    gpio_put(LED_PIN, 1);
+    printf("FAULT DETECTED - LED ON\n");
+
+    while (true) {
+        tight_loop_contents();
     }
 }
