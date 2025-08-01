88d96590d51950cc577780ff84f610ed1d736ff8
Zixuan Yin <yinzixuansz@gmail.com>
2025-08-01 16:06:20 -0400

    Fixed bugs

--- emfi-pico-ex/emfi-pico-ex.cpp
+++ emfi-pico-ex/emfi-pico-ex.cpp
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include "stdlib.h"
 
-#include "lib/driverlib/gpio.h"
+//#include "lib/driverlib/gpio.h"
 #include "pico/stdlib.h"
 #include "pico/time.h"
 
@@ -18,21 +18,23 @@ int main() {
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
     initialize_uarts();
+    uart_set_pin(UART1, )
     uart_init(UART0);
-    uart_init(UART0);
+    uart_init(UART1);
 
     uint64_t cnt = 1;
     const uint64_t inc = 1;
-    while (uart.read(UART0, 1, 0) != 'A') {
+    uint8_t buf;
+    while (uart_read_blocking(UART1, buf, 1) != 'A') {
         sleep_us(1);
     }
     while (true) {
         sleep_us(cnt);
         gpio_put(EMP_PIN, 1);
-        uint8_t ret = uart_read(UART1, 1, 0);
+        uint8_t ret = uart_read_blocking(UART1, buf, 1);
         if (ret == 'B') {
             for (int i = 0; i < 4; i++) {
-                uart_write(UART0, cnt & 0xFF);
+                uart_write_blocking(UART0, 'A', 1);
                 cnt >>= 2;
             }
             break;
